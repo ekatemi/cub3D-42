052b1cda35805afad5a05092a1a9ba43094f81a0
Ekaterina Mikaylova <ekaterinamikaylova@Ekaterinas-MacBook-Air.local>
2025-03-25 17:02:21 +0100

    fixed leaks, parse path and color

--- main.c
+++ main.c
@@ -34,7 +34,7 @@ void printInput(t_data data)
 
 
 //parse string like this "255,0,4" andd assign to F or C
-int fillColor(t_data *d, char *str, char id)
+int fillColor(t_location *l, char *str, char id)
 {
     int i;
     char **arr;
@@ -57,11 +57,11 @@ int fillColor(t_data *d, char *str, char id)
     if(id == 'C')
     {
         // Convert strings to numbers
-        d->C.r = ft_atoi(arr[0]);
-        d->C.g = ft_atoi(arr[1]);
-        d->C.b = ft_atoi(arr[2]);
+        l->r = ft_atoi(arr[0]);
+        l->g = ft_atoi(arr[1]);
+        l->b = ft_atoi(arr[2]);
 
-        if (d->C.r == -1 || d->C.g == -1 || d->C.b == -1) {
+        if (l->r == -1 || l->g == -1 || l->b == -1) {
             printf("Error: Invalid RGB value (must be 0-255 and only digits)\n");
             free_matrix(arr, 3);
             return (0); //err
@@ -72,11 +72,11 @@ int fillColor(t_data *d, char *str, char id)
     else if(id == 'F')
     {
         // Convert strings to numbers
-        d->F.r = ft_atoi(arr[0]);
-        d->F.g = ft_atoi(arr[1]);
-        d->F.b = ft_atoi(arr[2]);
+        l->r = ft_atoi(arr[0]);
+        l->g = ft_atoi(arr[1]);
+        l->b = ft_atoi(arr[2]);
 
-        if (d->F.r == -1 || d->F.g == -1 || d->F.b == -1) {
+        if (l->r == -1 || l->g == -1 || l->b == -1) {
             printf("Error: Invalid RGB value (must be 0-255 and only digits)\n");
             free_matrix(arr, 3);
             return (0); //err
@@ -84,96 +84,8 @@ int fillColor(t_data *d, char *str, char id)
         free_matrix(arr, 3);
         return (1); // Ok
     }
-        
     return 0;
 }
-
-// int extractColor(char *str, t_data *data)
-// {
-//     if (!str || !data)
-//         return (0);
-//     int i = 0;
-    
-//     while (ft_isspace(str[i])) //skip spaces at the beginning
-//     {
-//         i++;
-//     }
-
-//     if (ft_strncmp(&str[i], "F", 1) == 0 && )
-//     {
-//         if (data->F.r != -1)
-//         {
-//             printf("Error: duplicated identifier F\n");
-//             return (-1);
-//         }
-//         fillColor(&data->F, str[i]);
-//     }   
-//     else if (ft_strncmp(&str[i], "C", 1) == 0)
-//     {
-//         if (data->C.r != -1)
-//         {
-//             printf("Error: duplicated identifier C\n");
-//             return (-1);
-//         }
-//         fillColor(&data->C, str[i]);
-//     }
-//     else
-   
-//     //no valid identifier found
-//     if (!loc_target || ( str[i + 1] != ' ' && str[i + 1] != '\0'))
-//         return (0);
-//     i += 1;
-    
-//     while (ft_isspace(str[i]))
-//         i++;
-
-//     if (loc_target != NULL)//duplicated
-//     {
-//         printf("Error: duplicated identifier %s\n", identifier);
-//         return (-1);
-//     }
-
-//     loc_target = ft_strdup(&str[i]);
-
-//     return (1);
-// }
-
-
-
-
-//WORKING
-// int checkIdentifier(char *str, char **target, char *id) //id "NO"
-// {
-//     size_t id_len = ft_strlen(id); //len 2
-
-//     while (ft_isspace(*str))
-//         str++;
-//     //printf("str[0] is %c %c \n", str[0], str[1]);
-//     if (ft_strncmp(str, id, id_len) == 0 && ft_isspace(*(str + id_len)))
-//     {
-//         str += id_len;
-//         // Skip spaces
-//         while (ft_isspace(*str))
-//             str++;
-
-//         //printf("target is %s\n", *target);
-            
-//         if (*target != NULL)
-//         {
-//             printf("Error: Duplicated identifier %s\n", id);
-//             return (0); // Input error, return immediately
-//         }
-//         *target = ft_strdup(str);  // Assign the duplicated string to NO or SO
-
-//         if (!*target)
-//             return (0); // Allocation error
-
-//         return (1);  // Success
-//     }
-    
-//     printf("failed to match (check)\n");//no matches
-//     return (0);  // Return 0 if the identifier does not match
-// }
 
 int checkIdentifier(char *str, void *target, char *id) //id "NO", "SO", "WE", "EA", "C", "F"
 {
@@ -214,8 +126,11 @@ int checkIdentifier(char *str, void *target, char *id) //id "NO", "SO", "WE", "E
             t_location *color_target = (t_location *)target; // Cast the target to t_location pointer
 
             // Now you can access the fields of the t_location structure    
-            color_target->b = 23;
-            color_target->b = 13;
+            if (!fillColor(color_target, str, *id))
+            {
+                
+                return 0;
+            }
 
             printf("Color Target: R=%d, G=%d, B=%d\n", color_target->r, color_target->g, color_target->b);
            
@@ -239,29 +154,48 @@ int parseInput(char *str, t_data *data)
         return (0);
 
     // If it's a map line, return success
-    if (str[0] == '1')
+    if (str[0] == '1' && data->filled == 6)
+    {
+        //handle map
         return (1);//????????
+    }
+       
 
     if (checkIdentifier(str, &(data->NO), "NO"))
-        return (1);
+    {
+      data->filled += 1;
+      return (1);  
+    }    
     if (checkIdentifier(str, &(data->SO), "SO"))
-        return (1);
+    {
+      data->filled += 1;
+      return (1);  
+    }
     if (checkIdentifier(str, &(data->WE), "WE"))
-        return (1);
+    {
+      data->filled += 1;
+      return (1);  
+    }
     if (checkIdentifier(str, &(data->EA), "EA"))
-        return (1);
-    if (checkIdentifier(str, &(data->EA), "F"))
-        return (1);
-    if (checkIdentifier(str, &(data->EA), "C"))
-        return (1);   
-
+    {
+      data->filled += 1;
+      return (1);  
+    }
+    if (checkIdentifier(str, &(data->F), "F"))
+    {
+      data->filled += 1;
+      return (1);  
+    }
+    if (checkIdentifier(str, &(data->C), "C"))
+    {
+      data->filled += 1;
+      return (1);  
+    }  
     else
     {
         freeData(data);
         return(0);
     }
-        
-
     freeData(data);
     printf("Error: Unrecognized input: [%s]\n", str);
     return (0);
